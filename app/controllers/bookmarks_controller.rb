4ade8eff928b523452ca13aeb3e95f30037f7829
Raghu Betina <raghu@firstdraft.com>
2017-09-29 18:36:21 +0000

    Only bookmark user can modify bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,14 @@
 class BookmarksController < ApplicationController
+  before_action :current_user_must_be_bookmark_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_bookmark_user
+    bookmark = Bookmark.find(params[:id])
+
+    unless current_user == bookmark.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @bookmarks = Bookmark.all
 
